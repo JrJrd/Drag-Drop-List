2c7f5a9b71faa677844151edd46821346d62b959
atSeaer <121316243+atSeaer@users.noreply.github.com>
2023-05-19 11:22:24 +0300

    Start

---
+++ script.js
@@ -0,0 +1,15 @@
+const draggable_list = document.getElementById('draggable-list');
+
+
+const largestAnimals = [
+  'Blue Whale',
+  'African Elephant',
+  'Giraffe',
+  'Saltwater Crocodile',
+  'Polar Bear',
+  'Komodo Dragon',
+  'Giant Squid',
+  'Brown Bear',
+  'Hippopotamus',
+  'Siberian Tiger'
+];
