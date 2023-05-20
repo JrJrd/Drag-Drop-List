5de77a42790c3a2aa9664ad0ee7daf7eed7a3bfb
atSeaer <121316243+atSeaer@users.noreply.github.com>
2023-05-20 15:27:59 +0300

    Functions
    The createList function generates the list items by shuffling the largestAnimals
    
    The addEventListeners function adds event listeners to the draggable elements and list items.
    
    The dragStart, dragEnter, dragLeave, dragOver, and dragDrop functions handle the drag and drop functionality of the list items. They update the dragStartIndex variable and swap items when dropped.

--- script.js
+++ script.js
@@ -1,44 +1,93 @@
 const draggable_list = document.getElementById('draggable-list');
 
-
 const largestAnimals = [
   'Blue Whale',
   'African Elephant',
   'Giraffe',
   'Saltwater Crocodile',
-  'Polar Bear',
-  'Komodo Dragon',
+  'Kodiak Bear',
+  'Ostrich',
   'Giant Squid',
-  'Brown Bear',
-  'Hippopotamus',
-  'Siberian Tiger'
+  'Polar Bear',
+  'Gorilla',
+  'Anaconda'
 ];
 
+//  list items
 const listItems = [];
+
 let dragStartIndex;
+
 createList();
 
+//  list 
 function createList() {
-    [...largestAnimals]
-      .map(a => ({ value: a, sort: Math.random() }))
-      .sort((a, b) => a.sort - b.sort)
-      .map(a => a.value)
-      .forEach((animal, index) => {
-        const listItem = document.createElement('li');
-  
-        listItem.setAttribute('data-index', index);
-  
-        listItem.innerHTML = `
-          <span class="number">${index + 1}</span>
-          <div class="draggable" draggable="true">
-            <p class="animal-name">${animal}</p>
-            <i class="fas fa-grip-lines"></i>
-          </div>
-        `;
-  
-        listItems.push(listItem);
-  
-        draggable_list.appendChild(listItem);
-      });
-  
-  }
+  [...largestAnimals]
+    .map(a => ({ value: a, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(a => a.value)
+    .forEach((animal, index) => {
+      const listItem = document.createElement('li');
+      listItem.setAttribute('data-index', index);
+      listItem.innerHTML = `
+        <span class="number">${index + 1}</span>
+        <div class="draggable" draggable="true">
+          <p class="animal-name">${animal}</p>
+          <i class="fas fa-grip-lines"></i>
+        </div>
+      `;
+      listItems.push(listItem);
+      draggable_list.appendChild(listItem);
+    });
+
+
+}
+
+function dragStart() {
+  dragStartIndex = +this.closest('li').getAttribute('data-index');
+}
+
+function dragEnter() {
+  this.classList.add('over');
+}
+function dragLeave() {
+  this.classList.remove('over');
+}
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragDrop() {
+  const dragEndIndex = +this.getAttribute('data-index');
+  swapItems(dragStartIndex, dragEndIndex);
+
+  this.classList.remove('over');
+}
+
+// Swap 
+function swapItems(fromIndex, toIndex) {
+  const itemOne = listItems[fromIndex].querySelector('.draggable');
+  const itemTwo = listItems[toIndex].querySelector('.draggable');
+
+  listItems[fromIndex].appendChild(itemTwo);
+  listItems[toIndex].appendChild(itemOne);
+}
+
+
+function addEventListeners() {
+  const draggables = document.querySelectorAll('.draggable');
+  const dragListItems = document.querySelectorAll('.draggable-list li');
+
+  draggables.forEach(draggable => {
+    draggable.addEventListener('dragstart', dragStart);
+  });
+
+  dragListItems.forEach(item => {
+    item.addEventListener('dragover', dragOver);
+    item.addEventListener('drop', dragDrop);
+    item.addEventListener('dragenter', dragEnter);
+    item.addEventListener('dragleave', dragLeave);
+  });
+}
+
+
